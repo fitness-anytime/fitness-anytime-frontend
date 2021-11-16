8f3de8c30f64fe546ae22f9eff32910af54b681e
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-16 14:46:05 -0800

    Search category selector and search text input functional. Need to now render classes specific to search parameters

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ClassCard from "./ClassCard";
 import styled from "styled-components";
+import { TextField, MenuItem } from "@mui/material";
 
 const classCards = [
   {
@@ -53,25 +54,78 @@ const StyledUserContainer = styled.div`
   }
 `;
 
+const initialSearchParams = {
+  category: "",
+  value: ""
+}
+
 export default function User() {
   const [classes, setClasses] = useState(classCards);
+  const [searchParams, setSearchParams] = useState(initialSearchParams);
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setSearchParams({ ...searchParams, [event.target.name]: event.target.value})
+  }
+
   return (
     <>
-      <input placeholder="Search Bar" />
-      <StyledUserContainer>
+    {/* Search Bar Category Selector */}
+    <TextField
+      id="outlined-select-currency"
+      select
+      label="Category"
+      name="category"
+      value={searchParams.category}
+      onChange={handleChange}
+    >
+      <MenuItem key="startTime" value="startTime">
+        Time
+      </MenuItem>
+      <MenuItem key="classDate" value="classDate">
+        Date
+      </MenuItem>
+      <MenuItem key="duration" value="duration">
+        Duration
+      </MenuItem>
+      <MenuItem key="classType" value="classType">
+        Type
+      </MenuItem>
+      <MenuItem key="intensity" value="intensity">
+        Intensity (1-5)
+      </MenuItem>
+      <MenuItem key="location" value="location">
+        Location
+      </MenuItem>
+    </TextField>
+
+    {/* Search Bar Input */}
+    <TextField 
+      id="standard-basic" 
+      label="Search" 
+      variant="standard" 
+      name="value"
+      value={searchParams.value}
+      onChange={handleChange}
+    />    
+
+    <StyledUserContainer>
         {/* 
         --GET class data from API--
         --Map through class array to render to page here, inputting data as props--
       */}
         {classes.map((classData) => {
-          return (
-            <ClassCard
-              className="classCard"
-              key={classData.id}
-              isInstructor={false}
-              {...classData}
-            />
-          );
+          // if (classData[searchParams.category].includes(searchParams.value)) {
+            return (
+              <ClassCard
+                className="classCard"
+                key={classData.id}
+                isInstructor={false}
+                {...classData}
+              />
+            );
+          // }
+          // return null;
         })}
       </StyledUserContainer>
     </>
